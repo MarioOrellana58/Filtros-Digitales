3bc25046a8c5a213a8c9037414576cc3d29efe86
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2020-07-09 18:19:52 -0600

    Defined filters matrix written

---
+++ FiltrosDigitales-1020618/Arrays.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiltrosDigitales_1020618
+{
+    class Arrays
+    {
+        public double[,,] GetDefinedMatrix(string filterName)
+        {
+            switch (filterName)
+            {
+                case "blurred":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "enhance":
+                    return new double[,,]
+                                {
+                                    { { -2, -1, 0 } },
+                                    { { -1,  1, 1 } },
+                                    { {  0,  1, 2 } }
+                                };
+                case "lower sobel":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "upper sobel":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "left sobel":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "right sobel":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "outline":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "sharpen":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                case "original":
+                    return new double[,,]
+                                {
+                                    { { 0.0625, 0.125, 0.0625 } },
+                                    { { 0.125 , 0.25 , 0.125  } },
+                                    { { 0.0625, 0.125, 0.0625 } }
+                                };
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
