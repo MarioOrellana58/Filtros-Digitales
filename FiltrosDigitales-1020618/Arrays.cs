6493e14c57f0c04b6a72111534dd87fcf8141244
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2020-07-09 18:55:23 -0600

    Some initial operations developed, dimensions and typing errors fixed on defined matrix

--- FiltrosDigitales-1020618/Arrays.cs
+++ FiltrosDigitales-1020618/Arrays.cs
@@ -6,74 +6,74 @@ using System.Threading.Tasks;
 
 namespace FiltrosDigitales_1020618
 {
-    class Arrays
+    public class Arrays
     {
-        public double[,,] GetDefinedMatrix(string filterName)
+        public double[,] GetDefinedMatrix(string filterName)
         {
             switch (filterName)
             {
                 case "blurred":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     { 0.0625, 0.125, 0.0625 } ,
+                                     { 0.125 , 0.25 , 0.125  } ,
+                                     { 0.0625, 0.125, 0.0625 } 
                                 };
                 case "enhance":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { -2, -1, 0 } },
-                                    { { -1,  1, 1 } },
-                                    { {  0,  1, 2 } }
+                                     { -2, -1, 0 } ,
+                                     { -1,  1, 1 } ,
+                                     {  0,  1, 2 } 
                                 };
                 case "lower sobel":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     { -1, -2, -1 } ,
+                                     {  0,  0,  0 } ,
+                                     {  1,  2,  1 }
                                 };
                 case "upper sobel":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     {  1,  2,  1 } ,
+                                     {  0,  0,  0 } ,
+                                     { -1, -2, -1 }
                                 };
                 case "left sobel":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     { 1, 0, -1 } ,
+                                     { 2, 0, -2 } ,
+                                     { 1, 0, -1 }
                                 };
                 case "right sobel":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     { -1, 0, 1 } ,
+                                     { -2, 0, 2 } ,
+                                     { -1, 0, 1 }
                                 };
                 case "outline":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     { -1, -1, -1 } ,
+                                     { -1,  8, -1 } ,
+                                     { -1, -1, -1 }
                                 };
                 case "sharpen":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     {  0, -1,  0 } ,
+                                     { -1,  5, -1 } ,
+                                     {  0, -1,  0 }
                                 };
                 case "original":
-                    return new double[,,]
+                    return new double[,]
                                 {
-                                    { { 0.0625, 0.125, 0.0625 } },
-                                    { { 0.125 , 0.25 , 0.125  } },
-                                    { { 0.0625, 0.125, 0.0625 } }
+                                     { 0, 0, 0 } ,
+                                     { 0, 1, 0 } ,
+                                     { 0, 0, 0 }
                                 };
                 default:
                     break;
---
+++ FiltrosDigitales-1020618/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiltrosDigitales_1020618
+{
+    public class Calculator
+    {
+        /*
+         * Methods are not generic.
+         * I did it this way because it's way more efficient to work
+         * with a defined matrix dimension instead of making generic
+         * methods, this would rise code complexity.
+         */
+
+        public double[,] MatrixMultiplication(double[,] imagePortion, double[,] filter)
+        {
+            return new double[,]
+                            {
+                                { imagePortion[0,0]*filter[0,0], imagePortion[0,1]*filter[0,1], imagePortion[0,2]*filter[0,2] },
+                                { imagePortion[1,0]*filter[1,0], imagePortion[1,1]*filter[1,1], imagePortion[1,2]*filter[1,2] },
+                                { imagePortion[2,0]*filter[2,0], imagePortion[2,1]*filter[2,1], imagePortion[2,2]*filter[2,2] }
+                            };            
+        }
+
+        public double SumAll(double[,] matrix)
+        {
+            return matrix[0, 0] + matrix[0, 1] + matrix[0, 2] +
+                   matrix[0, 0] + matrix[0, 1] + matrix[0, 2] +
+                   matrix[0, 0] + matrix[0, 1] + matrix[0, 2];
+        }
+
+        public double StandardizeResult(double notStandardizedResult)
+        {
+            if (notStandardizedResult < 0)
+            {
+                return 0;
+            }
+            else if (notStandardizedResult > 200)
+            {
+                return 200;
+            }
+            else
+            {
+                if (notStandardizedResult % 1 != 0)
+                {
+                    return Math.Round(notStandardizedResult);
+                }
+                else
+                {
+                    //it's already standardized
+                    return notStandardizedResult;
+                }
+            }
+        }
+    }
+}
